e7253289b2287593b2b72b71e79be2e1aa18541e
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-20 16:37:02 -0600

    add components and update pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+// import NoMatch from './components/NoMatch';
+import NavBar from './components/Navbar';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 const App = () => (
-  <Route exact path="/" component={Home} />
+  <div>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+      {/* <Route component={NoMatch} /> */}
+    </Switch>
+  </div>
 );
+
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { isAuthenticated } from '../fakeAuth';
+
+const styles = {
+  active: {
+    textDecoration: 'underline',
+    fontWeight: 'bold',
+    color: 'black',
+  }
+}
+
+const additionalLinks = (history) => {
+  if (isAuthenticated()) {
+    return (
+      <span>
+        <NavLink activeStyle={styles.active} to="/dashboard">Dashboard</NavLink>
+        {' '}
+        <a href="" onClick={() => {
+          logout()
+          history.push("/login")
+        }}>
+          Logout
+        </a>
+      </span>
+    )
+  } else {
+    return (
+      <NavLink activeStyle={styles.active} to="/login">Login</NavLink>
+    )
+  }
+}
+
+const NavBar = ( history ) => (
+  <nav>
+    <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
+    {' '}
+    <NavLink activeStyle={styles.active} to="/about">About</NavLink>
+    {' '}
+    {additionalLinks(history)}
+  </nav>
+)
+
+export default withRouter(NavBar);
